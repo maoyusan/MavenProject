94f94ed413005d00aabf27a5859067b59e372a5f
Administrator <3183848638@qq.com>
2021-11-01 17:51:05 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Administrator <3183848638@qq.com>
    
    # Conflicts:
    #	myBatis-03/src/main/java/pojo/Blog.java

--- myBatis-03/src/test/java/mapper/BlogMapperTest.java
+++ myBatis-03/src/test/java/mapper/BlogMapperTest.java
@@ -45,7 +45,6 @@ public class BlogMapperTest extends TestCase {
         ArrayList<Object> objects = new ArrayList<>();
         objects.add(1);
         objects.add(2);
-        objects.add(3);
         HashMap<Object, Object> map = new HashMap<>();
         map.put("list", objects);
         List<Blog> blogs = mapper.queryBlog1(map);
