ff2d03637426f40c0798601084a7f90b7de281db
Administrator <3183848638@qq.com>
2021-11-01 16:37:48 +0800

    Merge branch 'master' into master
    Signed-off-by: Administrator <3183848638@qq.com>

--- myBatis-03/src/main/java/pojo/Blog.java
+++ myBatis-03/src/main/java/pojo/Blog.java
@@ -3,7 +3,6 @@ package pojo;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
 
 @Data
 public class Blog implements Serializable {
@@ -14,11 +13,5 @@ public class Blog implements Serializable {
     private java.sql.Timestamp createTime;
     private long views;
 
-    public Blog(String id, String title, String author, Timestamp createTime, long views) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.createTime = createTime;
-        this.views = views;
-    }
+
 }
