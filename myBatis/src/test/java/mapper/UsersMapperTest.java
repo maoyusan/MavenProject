ea25c1e72b9e495ea176918c6a6b8c4f6581baa9
Administrator <3183848638@qq.com>
2021-11-01 15:43:00 +0800

    test33
    Signed-off-by: Administrator <3183848638@qq.com>

--- myBatis/src/test/java/mapper/UsersMapperTest.java
+++ myBatis/src/test/java/mapper/UsersMapperTest.java
@@ -13,7 +13,7 @@ public class UsersMapperTest extends TestCase {
     private final SqlSession sqlSession;
 
     {
-        //test
+        //test11
         sqlSession = MybatisUtils.getSqlSession();
         mapper = sqlSession.getMapper(UsersMapper.class);
     }
